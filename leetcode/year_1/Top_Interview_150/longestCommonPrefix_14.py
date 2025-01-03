0dd202ed1a03153b00a608a01d2af3f33e219267
Chris-Coleongco <christien.coleongco@gmail.com>
2025-01-02 23:48:52 -0700

    another one hehe

--- leetcode/year_1/Top_Interview_150/longestCommonPrefix_14.py
+++ leetcode/year_1/Top_Interview_150/longestCommonPrefix_14.py
@@ -1,8 +1,35 @@
 def longestCommonPrefix(strs: list[str]) -> str:
 
+    shortest_str_len = float("inf") # initialize as inf so if shortest is ever returned as -1 to next logic, we know some weird ass error occurred
+
     for i in range(len(strs)):
-        len(strs[i])
-        if  < 
+        if shortest_str_len > len(strs[i]):
+            shortest_str_len = len(strs[i])
+
+    longest_pref = ""
+
+    if isinstance(shortest_str_len, int):
+        for i in range(shortest_str_len):
+
+            old = strs[0][i] # init with first char
+
+            same = True
+
+            for s in strs:
+
+                if s[i] != old:
+                    # not same and should not be appended. cut off
+                    same = False
+
+
+            if same == True:
+                longest_pref += old
+            else:
+                break
+
+        return longest_pref
+    else:
+        return ""
 
         
 def main():
